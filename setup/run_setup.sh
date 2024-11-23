d467e6a3a8df913e78dc6ffda99155df7966af05
pascalweiss <mail@pascal-weiss.de>
2024-11-23 17:42:47 +0100

    added quotes to various files

--- setup/run_setup.sh
+++ setup/run_setup.sh
@@ -3,69 +3,69 @@
 set +e
 
 DIR="$( cd "$(dirname "$0")/.." ; pwd -P )"
-SETUP_OS=$DIR/setup/_func_packages_os.sh
-SETUP_PY=$DIR/setup/_func_packages_python.sh
-SETUP_OH_MY_ZSH=$DIR/setup/_func_oh_my_zsh.sh
-SETUP_DOTFILES=$DIR/setup/_func_dotfiles.sh
+SETUP_OS="$DIR/setup/_func_packages_os.sh"
+SETUP_PY="$DIR/setup/_func_packages_python.sh"
+SETUP_OH_MY_ZSH="$DIR/setup/_func_oh_my_zsh.sh"
+SETUP_DOTFILES="$DIR/setup/_func_dotfiles.sh"
 ARGS=("${@}")
 
-source $DIR/setup/_func_preparation.sh
-source $DIR/executables/bin/commons
+source "$DIR/setup/_func_preparation.sh"
+source "$DIR/executables/bin/commons"
 
 # Add files to config folder
-mkdir $DIR/config
-FILES=($(ls $DIR/setup/templates))
+mkdir "$DIR/config"
+FILES=($(ls "$DIR/setup/templates"))
 for FILE in "${FILES[@]}"; do 
     cp "$DIR/setup/templates/$FILE" "$DIR/config/.$FILE"; 
 done
 
 # Install dependencies
 if ! contains "--update-package-manager=no" "${ARGS[@]}"; then 
-    source $SETUP_OS
+    source "$SETUP_OS"
     install_curl
     update_package_manager
 fi
 
 # Install dependencies
-if ! contains "--install-dependencies=no" "${ARGS[@]}"; then 
-    source $SETUP_OS && install_dependencies
+if ! contains "--install-dependencies=no" "${ARGS[@]}"; then
+    source "$SETUP_OS" && install_dependencies
 fi
 
 # Install OS packages
 function lambda_os_install () {
-    source $SETUP_OS && install_packages
+    source "$SETUP_OS" && install_packages
 }
 install_dialog "--install-os-packages" "--install-os-packages=no" "Do you want to install the OS packages (config/packages_os.txt)? Type (Y/n): " "lambda_os_install" "${ARGS[@]}"
 
 # Install python packages
 function lambda_python_install () {
-    source $SETUP_OS && install_python
-    source $SETUP_PY && install_packages
+    source "$SETUP_OS" && install_python
+    source "$SETUP_PY" && install_packages
 }
 install_dialog "--install-python-packages" "--install-python-packages=no" "Do you want to install the python packages (config/packages_python.txt)? Type (Y/n): " "lambda_python_install" "${ARGS[@]}"
 
 # Install oh-my-zsh
 function lambda_oh_my_zsh_install () {
-    source $SETUP_OS && install_oh_my_zsh
-    source $SETUP_OH_MY_ZSH && install_packages
+    source "$SETUP_OS" && install_oh_my_zsh
+    source "$SETUP_OH_MY_ZSH" && install_packages
 }
 install_dialog "--install-oh-my-zsh" "--install-oh-my-zsh=no" "Do you want to install oh-my-zsh? Type (Y/n): " "lambda_oh_my_zsh_install" "${ARGS[@]}"
 
 # Replace Shell bashrc
 function lambda_replace_bashrc () {
-    source $SETUP_DOTFILES && replace_bashrc
+    source "$SETUP_DOTFILES" && replace_bashrc
 }
 install_dialog "--replace-bashrc" "--replace-bashrc=no" "The next step will backup your .bashrc in ~/dotfiles_backup and replace it with a new one. Do you want this? Type (Y/n): " "lambda_replace_bashrc" "${ARGS[@]}"
 
 # Replace Shell zshrc
 function lambda_replace_zshrc () {
-    source $SETUP_DOTFILES && replace_zshrc
+    source "$SETUP_DOTFILES" && replace_zshrc
 }
 install_dialog "--replace-zshrc" "--replace-zshrc=no" "The next step will backup your .zshrc in ~/dotfiles_backup and replace it with a new one. Do you want this? Type (Y/n): " "lambda_replace_zshrc" "${ARGS[@]}"
 
 # Replace vim config
 function lambda_replace_vim () {
-    source $SETUP_DOTFILES && replace_vim
+    source "$SETUP_DOTFILES" && replace_vim
 }
 install_dialog "--replace-vim" "--replace-vim=no" "The next step will backup your .vimrc and .vim in ~/dotfiles_backup and replace them with a new ones. Do you want this? Type (Y/n): " "lambda_replace_vim" "${ARGS[@]}"
 
@@ -84,9 +84,9 @@ done
 
 
 if contains "--install-oh-my-zsh=no" "${ARGS[@]}"; then
-    chsh --shell $(command -v zsh)
+    chsh --shell "$(command -v zsh)"
     exec bash
 else 
-    chsh --shell $(command -v zsh)
+    chsh --shell "$(command -v zsh)"
     exec zsh
 fi
