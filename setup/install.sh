1fee00d04c7f867b316c1f153fd756599e5a4ecd
pascalweiss <mail@pascal-weiss.de>
2024-11-24 14:26:08 +0100

    applx pipx

--- setup/install.sh
+++ setup/install.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
+source "$DIR"/setup/_func_console_output.sh
+
 mkdir "$HOME"/tmp
+echo -e "${YELLOW}Cloning envi-shell${NC}"
 git clone --recurse-submodules https://github.com/pascalweiss/envi-shell.git "$HOME"/tmp/envi
 mv "$HOME"/tmp/envi "$HOME"/.envi
 rm -rf "$HOME"/tmp/envi
