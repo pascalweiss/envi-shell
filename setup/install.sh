e9f74b3daab6279423c0b72445090d8ff7e1515e
pascalweiss <mail@pascal-weiss.de>
2024-11-24 11:39:12 +0100

    more output

--- setup/install.sh
+++ setup/install.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-mkdir $HOME/tmp
-git clone --recurse-submodules https://github.com/pascalweiss/envi-shell.git $HOME/tmp/envi
-mv $HOME/tmp/envi $HOME/.envi
-rm -rf $HOME/tmp/envi
+mkdir "$HOME"/tmp
+git clone --recurse-submodules https://github.com/pascalweiss/envi-shell.git "$HOME"/tmp/envi
+mv "$HOME"/tmp/envi "$HOME"/.envi
+rm -rf "$HOME"/tmp/envi
 
-$HOME/.envi/setup/run_setup.sh
+"$HOME"/.envi/setup/run_setup.sh
--- setup/run_setup.sh
+++ setup/run_setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set +e
 
 DIR="$( cd "$(dirname "$0")/.." ; pwd -P )"
 SETUP_OS="$DIR/setup/_func_packages_os.sh"
