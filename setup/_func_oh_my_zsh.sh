4af38d114cec696a732d84635c05e12e69573fcb
pascalweiss <4643156+pascalweiss@users.noreply.github.com>
2025-01-27 18:11:05 +0100

    Added some defaults

--- setup/_func_oh_my_zsh.sh
+++ setup/_func_oh_my_zsh.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 function install_packages () {
-    
     curl -Lo /tmp/oh_my_install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
     sh /tmp/oh_my_install.sh --unattended
     rm /tmp/oh_my_install.sh
