310e830e0c2f22382ce4c71a103d50a136ad02a5
pascalweiss <mail@pascal-weiss.de>
2025-01-27 20:32:12 +0100

    Add zsh autosuggestions

--- setup/_func_oh_my_zsh.sh
+++ setup/_func_oh_my_zsh.sh
@@ -4,4 +4,7 @@ function install_packages () {
     curl -Lo /tmp/oh_my_install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
     sh /tmp/oh_my_install.sh --unattended
     rm /tmp/oh_my_install.sh
+
+    # Also install autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
